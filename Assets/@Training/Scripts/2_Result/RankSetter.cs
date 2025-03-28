c4d8ee815e18d38d015618b8fcdab37834e1f7fb
CCS-WIN-111\Junpei Yoshino <Junpei Yoshino>
2025-03-28 17:41:29 +0900

    a

--- Assets/@Training/Scripts/2_Result/RankSetter.cs
+++ Assets/@Training/Scripts/2_Result/RankSetter.cs
@@ -62,11 +62,13 @@ public class RankSetter : MonoBehaviour
     {
         // プレイヤーのスコアを呼び出す
         scorePlayer = PlayerPrefs.GetFloat(Player_Score_Key);
+
         if (scorePlayer <= 0f) {
             TXTPlayerScore.text = "_._s";
         } else {
             TXTPlayerScore.text = $"{scorePlayer:f1}s";
         }
+
         TXTPlayerScore.color = Color.red;
 
         if (PlayerPrefs.HasKey(Rank1_Score_Key)) {
